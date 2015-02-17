c78cf3c5205928567f476cc8418878f466d4d0a4
unknown <nam14@PSVWS551.pau.local>
2015-02-17 09:44:45 +0000

    added play class

---
+++ src/Game/Play.java
@@ -0,0 +1,17 @@
+package Game;
+
+public class Play {
+
+	public static void main(String[] args) {
+		playGame();
+
+	}
+	
+	public static void playGame() {
+		
+		for(int i = 0; i < 10; i++){
+			
+		}
+	}
+
+}
